73c2e2492d86ba6bd05cf0780913cad31e6ad803
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2022-12-01 19:15:52 -0700

    Update PMConfiguration.py

--- PMConfiguration.py
+++ PMConfiguration.py
@@ -6,3 +6,4 @@ load3 = "YOUR LOAD 3 NAME:"  #load connected to the 0x42 terminals
 cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
 certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
 keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
+timezone = 'US/Arizona' #timezone for timestamps, consult pytz list of timezones for acceptable timezones
