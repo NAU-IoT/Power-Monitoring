38e0b792231559d2305e93682ceb6cee65c85834
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2022-12-01 17:48:45 -0700

    Update PMConfiguration.py

--- PMConfiguration.py
+++ PMConfiguration.py
@@ -1,6 +1,8 @@
-topic = "YOUR TOPIC NAME"
-broker = "YOUR BROKER IP/HOSTNAME" 
+topic = "YOUR TOPIC NAME" #topic name
+broker = "YOUR BROKER IP/HOSTNAME" #broker name
 load1 = "YOUR LOAD 1 NAME:"  #load connected to 0x40 terminals
 load2 = "YOUR LOAD 2 NAME:"  #load connected to the 0x41 terminals
 load3 = "YOUR LOAD 3 NAME:"  #load connected to the 0x42 terminals
-
+cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
+certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
+keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
