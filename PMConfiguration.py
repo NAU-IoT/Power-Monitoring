b156130cef2fdd98fdc0cfad5c4ed4a30e2c36fc
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2022-12-01 17:40:11 -0700

    Create PMConfiguration.py

---
+++ PMConfiguration.py
@@ -0,0 +1,6 @@
+topic = "YOUR TOPIC NAME"
+broker = "YOUR BROKER IP/HOSTNAME" 
+load1 = "YOUR LOAD 1 NAME:"  #load connected to 0x40 terminals
+load2 = "YOUR LOAD 2 NAME:"  #load connected to the 0x41 terminals
+load3 = "YOUR LOAD 3 NAME:"  #load connected to the 0x42 terminals
+
