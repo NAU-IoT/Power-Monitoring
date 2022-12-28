8aa4de2f014cbb92865748ed93a42c83ba8d26d2
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2022-12-28 14:35:10 -0700

    Update PowerMonitor.sh

--- Systemctl/PowerMonitor.sh
+++ Systemctl/PowerMonitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # cd /code/python/myapp
-python3 /SOME/PATH/TO/PowerMonitor.py >> /SOME/PATH/TO/logs/PowerMonitor.log 2>&1
+python3 /SOME/PATH/TO/PowerMonitor.py >> /SOME/PATH/TO/logs/PowerMonitor-`date '+%Y%m%d'`.log 2>&1
