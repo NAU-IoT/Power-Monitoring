9049a3e109d07d1a786e4131eeedb88672950117
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-03-14 11:29:41 -0700

    Update PMConfiguration.py

--- PMConfiguration.py
+++ PMConfiguration.py
@@ -1,5 +1,5 @@
 topic = "PowerMonitoring" #topic name
-datastorage = "/SOME/PATH/TO/Data/" #path to store the local csv files
+datastorage = "/SOME/PATH/TO/Data/" #path to store the local csv files, make storage directories ahead of time
 broker = "YOUR BROKER NAME" #broker name or broker IP
 load1 = "YOUR LOAD 1"  #load connected to 0x40 terminals
 load2 = "YOUR LOAD 2"           #load connected to the 0x41 terminals
