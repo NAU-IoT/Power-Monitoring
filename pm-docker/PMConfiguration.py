0dd443b7e117015b0f0fd84faa9a6e4ce3842892
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-03-17 15:55:24 -0700

    Update PMConfiguration.py

--- pm-docker/PMConfiguration.py
+++ pm-docker/PMConfiguration.py
@@ -1,5 +1,5 @@
 topic = "PowerMonitoring" #topic name
-port = "YOUR_MQTT_PORT_NUMBER" #port to be used by mqtt
+port = YOUR_MQTT_PORT_NUMBER #port to be used by mqtt
 datastorage = "/SOME/PATH/TO/Data/" #path to store the local csv files, make storage directories ahead of time
 broker = "YOUR BROKER NAME" #broker name or broker IP
 load1 = "YOUR LOAD 1"  #load connected to 0x40 terminals
